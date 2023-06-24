c318352ec495dd28ab541ac1a3ec5983b678cf07
unknown <kuyetdev.forwork@gmail.com>
2023-06-24 10:38:10 +0700

    create branch

--- GroceryStore/app/src/main/java/com/example/grocerystore/LoginActivity.java
+++ GroceryStore/app/src/main/java/com/example/grocerystore/LoginActivity.java
@@ -29,7 +29,7 @@ import java.util.HashSet;
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
-
+//
     //UI views
     private EditText edtEmail, edtPassword;
     private TextView tvForgot, noAccountTv;
